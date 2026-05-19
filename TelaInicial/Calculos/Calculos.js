2e98476af3c8a62179b0ff40e292a3bc17db6d38
Julio Cesar <marciano.julinho@gmail.com>
2026-05-19 19:20:05 -0300

    mench

--- TelaInicial/Calculos/Calculos.js
+++ TelaInicial/Calculos/Calculos.js
@@ -1,13 +1,7 @@
 const idLogado = localStorage.getItem("usuarioId");
 const res = await fetch(`https://69ec4ad897482ad5c528424e.mockapi.io/usuario/${idUsuario}`);
 const data = await res.json();
-
-
-
-async function trazerValores(){
-    var ValorEmEnergia = data.energiaTotal 
-
-}
+var ValorEmEnergia = data.energiaTotal 
 
 
 
